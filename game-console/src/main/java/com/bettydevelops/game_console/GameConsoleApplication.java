5e17c015e184c6372923018e7992b2f1265cbeb2
TTGINT\betul.tultay <betul.tultay@ttgint.com>
2024-08-15 16:20:12 +0300

    First commit

---
+++ game-console/src/main/java/com/bettydevelops/game_console/GameConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.bettydevelops.game_console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameConsoleApplication.class, args);
+	}
+    //First commit
+}
---
+++ game-console/src/test/java/com/bettydevelops/game_console/GameConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bettydevelops.game_console;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
